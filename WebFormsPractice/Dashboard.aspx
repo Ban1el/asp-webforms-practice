c563c6726363c86f8524d36638cdbf8ffec3def3
Ban1el <danieltroyaromin1337@gmail.com>
2024-06-12 21:43:54 +0800

    Files added

---
+++ WebFormsPractice/Dashboard.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Dashboard.aspx.cs" Inherits="WebFormsPractice._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <main>
+        <div>
+            <asp:DropDownList ID="ddlEmployee" runat="server">
+                <asp:ListItem Enabled="true" Text="Select Employee" Value="-1"></asp:ListItem>
+            </asp:DropDownList>
+        </div>
+    </main>
+
+</asp:Content>
---
+++ WebFormsPractice/Dashboard.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFormsPractice
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataSet DS = new DataSet();
+            string strPhysicalPath = Server.MapPath("Countries.xml");
+            DS.ReadXml(strPhysicalPath);
+            ddlEmployee.DataTextField = "CountryName";
+            ddlEmployee.DataValueField = "CountryId";
+            ddlEmployee.DataSource = DS;
+            ddlEmployee.DataBind();
+
+            ListItem li = new ListItem("Select", "-1");
+            ddlEmployee.Items.Insert(0, li);
+        }
+    }
+}
+
+
+
+#region Comments
+//From DB
+//string conn = ConfigurationManager.ConnectionStrings["DBConn"].ConnectionString;
+//using (SqlConnection con = new SqlConnection(conn))
+//{
+//    SqlCommand cmd = new SqlCommand("SELECT * FROM Employees", con);
+//    con.Open();
+//    ddlEmployee.DataSource = cmd.ExecuteReader();
+//    ddlEmployee.DataTextField = "first_name";
+//    ddlEmployee.DataValueField = "id";
+//    ddlEmployee.DataBind();
+//}
+
+//From XML
+//DataSet DS = new DataSet();
+//string strPhysicalPath = Server.MapPath("Countries.xml");
+//DS.ReadXml(strPhysicalPath);
+//ddlEmployee.DataTextField = "CountryName";
+//ddlEmployee.DataValueField = "CountryId";
+//ddlEmployee.DataSource = DS;
+//ListItem li = new ListItem("Select", "-1");
+//ddlEmployee.Items.Insert(0, li);
+//ddlEmployee.DataBind();
+#endregion
